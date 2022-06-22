de5d1d8567e84e0cb4a48a80d67feb981e2d77ad
ricx <66686454+zelbeus@users.noreply.github.com>
2022-06-22 12:24:27 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,7 +1,14 @@
 data = {}
-TriggerEvent("redemrp_inventory:getData",function(call)
-    data = call
-end)
+local QBRItems
+local framework = "redemrp" --"redemrp" or "qbr"
+
+if framework == "redemrp" then
+	TriggerEvent("redemrp_inventory:getData",function(call)
+	    data = call
+	end)
+elseif framework == "qbr" then 
+	QBRItems = exports['qbr-core']:GetItems()
+end
 
 
 RegisterServerEvent("ricxmas_gift:addgift")
@@ -9,6 +16,7 @@ AddEventHandler("ricxmas_gift:addgift", function()
 	local _source = source
     local label
     local text = ""
+if framework == "redemrp" then
     TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
         if user ~= nil then
             local random = math.random(1,#Config.Gifts)
@@ -25,6 +33,21 @@ AddEventHandler("ricxmas_gift:addgift", function()
             TriggerClientEvent("Notification:left_xmas", _source, Config.Title, text, "scoretimer_textures", "scoretimer_generic_tick", 2000)
         end
     end)
+elseif framework == "qbr" then 
+     local random = math.random(1,#Config.Gifts)
+     if  Config.Gifts[random][1] ~= "money" then
+          local itemData = QBRItems[Config.Gifts[random][1]]
+ 	  local User = exports['qbr-core']:GetPlayer(_source)
+	  User.Functions.AddItem(Config.Gifts[random][1], Config.Gifts[random][2])
+          local label = itemData.label
+          text = Config.Recieved.."\n"..label.." ("..Config.Gifts[random][2]..")"
+     else
+          local User = exports['qbr-core']:GetPlayer(_source)
+          User.Functions.AddMoney("cash", Config.Gifts[random][2]), "desc")
+          text = Config.Recieved.."\n+$"..Config.Gifts[random][2]
+     end
+          TriggerClientEvent("Notification:left_xmas", _source, Config.Title, text, "scoretimer_textures", "scoretimer_generic_tick", 2000)
+end
 end)
 
 
